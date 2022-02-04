3e7571744dd1914885e084ac8dd4f2b9f1e84faa
guizech <guizech@gmail.com>
2022-02-04 16:00:43 -0300

    Inclusão do programa #2

---
+++ Programas em Python/Calcular área da divisória, volume total, volume por compartimento e a área do material necessário para uma caixa com divisória na diagonal.py
@@ -0,0 +1,17 @@
+Comprimento = (float(input("Digite o valor do comprimento em mm: ")))/100
+Largura = (float(input("Digite o valor da largura em mm: ")))/100
+Altura = (float(input("Digite o valor da altura em mm: ")))/100
+areadaCaixa = ((Comprimento*Largura) + (2*Largura*Altura) + (2*Comprimento*Altura))
+volumetotal = Comprimento*Largura*Altura
+volumeporcompartimento = volumetotal/2
+diagonal = (((Comprimento**2)+(Largura**2))**(1/2))
+areaDivisoria = (diagonal*Altura)
+materialnecessario = areaDivisoria + areadaCaixa
+print("CARACTERÍSTICAS DA CAIXA:")
+print("Altura: ", Altura,"m")
+print("Largura: ", Largura,"m")
+print("Comprimento: ", Comprimento,"m")
+print("Área da divisória: ",(areaDivisoria),"m2")
+print("Volume Total: ",(volumetotal),"m3")
+print("Volume por compartimento: ", (volumeporcompartimento),"m3")
+print("Área de material necessário : ",(materialnecessario),"m2")
